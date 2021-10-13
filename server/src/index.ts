1003d43a4c7508dac3fc0b01e8bc793966d8b39b
Dan Soutar <dan.soutar@littlerocket.io>
2021-10-12 21:38:43 -0400

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express'
+import { schema } from './graphql'
+
+
+const app = express();
+const port = 9000;
+
+
+const startServer = async () => {
+    const server = new ApolloServer({ schema });
+    await server.start();
+    server.applyMiddleware({ app, path: '/api' });
+}
+startServer();
+
+
+app.listen(port, () => console.log(`app is running at http://localhost:${port} `));
