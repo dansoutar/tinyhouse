830fce1ac117ac4a016602aa2908c2b3ef8b8685
Dan Soutar <dan.soutar@littlerocket.io>
2021-10-13 08:50:55 -0400

    updated graphql api to use Schema Definition Language

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express'
-import { schema } from './graphql'
+import { typeDefs, resolvers } from './graphql/'
 
 
 const app = express();
@@ -8,7 +8,7 @@ const port = 9000;
 
 
 const startServer = async () => {
-    const server = new ApolloServer({ schema });
+    const server = new ApolloServer({ typeDefs, resolvers });
     await server.start();
     server.applyMiddleware({ app, path: '/api' });
 }
